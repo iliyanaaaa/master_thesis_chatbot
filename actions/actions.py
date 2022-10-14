a77457555eb4e0d1c990862a089ab2850d3f98cb
iliyanaaaa <nospliss@gmail.com>
2022-10-14 09:45:45 +0200

    Fix bugs for local data access;  delete unnecessary code; improve messages for when the data is not available in the DB; ask_exam_type, ans_register_class and ask_class_about into one intent

--- actions/actions.py
+++ actions/actions.py
@@ -11,6 +11,7 @@ from typing import Any, Text, Dict, List
 import torch
 
 from rasa_sdk import Tracker, FormValidationAction
+from rasa.core.actions.forms import FormAction
 from rasa_sdk.types import DomainDict
 
 from rasa_sdk import Action, Tracker
@@ -31,10 +32,18 @@ from actions.sql_queries import get_class_start_time_query, get_available_class_
     get_class_types_query, get_weekday_en_de, get_class_is_about, get_types_of_classes, \
     add_class_type_query, class_info_view_txt_query_class_name
 
-MODE = 'database'  # 'local_csv'
+MODE = 'local_csv'  # 'local_csv' 'database'
 sqa_con = SQA_CONN_PUB
 
 PSY_CONN_PUB.autocommit = True
+
+VARIABLES_DICT = {'class_types': 'class_type',
+                  'all_classes': ' class_name, class_type',
+                  'start_time': ' class_name, class_type, start_time, week_day',
+                  'lecturer': ' lecturer, week_day',
+                  'location': ' room, floor , building , facility , address, week_day',
+                  'moodle_link': 'hyperlink, week_day',
+                  'class_is_about': 'txt'}
 
 
 def perform_query(mode, looking_for, class_name=None, class_type=None, weekday=None, con=None):
@@ -56,20 +65,22 @@ def perform_query(mode, looking_for, class_name=None, class_type=None, weekday=N
         df = pd.read_sql(query, con=con)
     elif mode == 'local_csv':
         if looking_for == 'weekdays':
-            df = pd.read_csv('data/csv_exports/weekdays.csv')
+            df = pd.read_csv('C:/Users/tarpo/PycharmProjects/master_thesis_chatbot/data/csv_exports/weekdays.csv')
         elif looking_for == 'types_of_classes':
-            df = pd.read_csv('data/csv_exports/types_of_classes.csv')
+            df = pd.read_csv('C:/Users/tarpo/PycharmProjects/master_thesis_chatbot/data/csv_exports/types_of_classes.csv')
         else:
-            df = pd.read_csv('data/csv_exports/class_info_view.csv')
+            df = pd.read_csv('C:/Users/tarpo/PycharmProjects/master_thesis_chatbot/data/csv_exports/class_info_view_txt.csv')
             variables_str = VARIABLES_DICT.get(looking_for)
             variables = variables_str.split(',')
-            if class_type is not None:
+            variables = [x.strip(' ') for x in variables]
+            print('class name:', class_name, 'class type:', class_type, 'week day:', weekday)
+            if class_name is not None:
                 df = df[(df['class_name'] == class_name)]
             if class_type is not None:
                 df = df[(df['class_type'] == class_type)]
             if weekday is not None:
-                df = df[(df['weekday'] == weekday)]
-            df = df[variables]
+                df = df[(df['week_day'] == weekday)]
+            df = df[variables].drop_duplicates(ignore_index=True)
 
     return df
 
@@ -81,15 +92,33 @@ CLASS_TYPE_MAPPINGS_df = perform_query(MODE, 'types_of_classes', con=sqa_con)
 WEEKDAYS_EN_DE = dict(perform_query(MODE, 'weekdays', con=sqa_con).values)
 WEEKDAYS_DE_EN = {y: x for x, y in WEEKDAYS_EN_DE.items()}
 # CLASS_TYPES_EN_DE = dict(pd.read_sql(get_class_types_query, con=SQA_CONN_PUB).values)
-CLASS_TYPES_DE_EN = {'Seminar': 'seminar', 'Übung': 'exercise', 'Vorlesung': 'lecture', 'Grundkurs': 'ground course',
-                     'Sprachkurs': 'language course', 'Hauptseminar': 'main seminar'}
+CLASS_TYPES_DE_EN = {"Seminar": "seminar",
+                     "Übung": "exercise",
+                     "Vorlesung": "lecture",
+                     "Grundkurs": "ground course",
+                     "Sprachkurs": "language course",
+                     "Hauptseminar": "main seminar",
+                     "Basiskurse":"base course",
+                     "Colloquium":"colloquim",
+                     "Forschungsseminar":"practice seminar",
+                     "Integrierter Theorie- und Praxiskurs":"integrated theorie and practice course",
+                     "Klausurenkurs":"exam course",
+                     "Kolloquium":"colloquim",
+                     "Masterseminar":"master seminar",
+                     "Oberseminar":"senior seminar",
+                     "Praktikum":"internship",
+                     "Praxisorientierte Lehrveranstaltung":"Practice-oriented course",
+                     "Projektseminar":"project seminar",
+                     "Proseminar":"pro seminar",
+                     "Seminar/Hauptseminar":"seminar/main seminar",
+                     "Seminar/Proseminar":"seminar/pro seminar",
+                     "Studienprojekt":"study project",
+                     "Tutorium":"tutorial",
+                     "Vertiefungskurse":"advanced course",
+                     "Vorlesung/Grundkurs":"lecture/ground course",
+                     "Vorlesung/Übung":"lecture/exercise"
+                     }
 CLASS_TYPES_EN_DE = {y: x for x, y in CLASS_TYPES_DE_EN.items()}
-VARIABLES_DICT = {'class_types': 'class_type',
-                  'start_time': ' class_name, class_type, start_time, week_day',
-                  'lecturer': ' lecturer',
-                  'location': ' room, floor , building , facility , address',
-                  'moodle_link': 'hyperlink',
-                  'class_is_about': 'txt'}
 
 LANG = 'en'
 
@@ -131,7 +160,7 @@ class ValidateClassNameTypeForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `class_name` value."""
 
-        print('start validating class name')
+        print('start validating class type name')
         if slot_value not in CLASS_NAMES:
             similarity_perc = [Levenshtein.jaro(slot_value.lower(), s.lower()) for s in CLASS_NAMES]
             if max(similarity_perc) < 0.7:
@@ -151,8 +180,13 @@ class ValidateClassNameTypeForm(FormValidationAction):
             domain: DomainDict,
     ) -> Dict[Text, Any]:
         """Validate `class_type` value."""
+
+        print('start validating class type:', class_type)
+        print('class type in CLASS_TYPES:', class_type in CLASS_TYPES_EN_DE.keys())
         class_name = tracker.get_slot('class_name')
-        if class_type not in CLASS_TYPES:
+        if class_type in CLASS_TYPES_EN_DE.keys():
+            class_type = CLASS_TYPES_EN_DE.get(class_type)
+        elif class_type not in CLASS_TYPES:
             similarity_perc = [Levenshtein.jaro(class_type.lower(), s.lower()) for s in CLASS_TYPES]
             if max(similarity_perc) < 0.7:
                 dispatcher.utter_message(text=f"I don't recognize that class type. You could choose from "
@@ -161,11 +195,11 @@ class ValidateClassNameTypeForm(FormValidationAction):
             else:
                 most_similar_index = similarity_perc.index(max(similarity_perc))
                 class_type = CLASS_TYPES[most_similar_index]
-        if class_type not in ['lecture', 'exercise', 'seminar']:
-            class_type = \
-                CLASS_TYPE_MAPPINGS_df.loc[CLASS_TYPE_MAPPINGS_df['abbr'] == class_type][
-                    'class_type_de'].values.tolist()[
-                    0]
+        # if class_type not in ['lecture', 'exercise', 'seminar']:
+        #     class_type = \
+        #         CLASS_TYPE_MAPPINGS_df.loc[CLASS_TYPE_MAPPINGS_df['abbr'] == class_type][
+        #             'class_type_de'].values.tolist()[
+        #             0]
         df = perform_query(mode=MODE, looking_for='start_time', class_name=class_name,
                            class_type=class_type, con=sqa_con)
         if df.empty:
@@ -187,6 +221,7 @@ class ValidateClassNameForm(FormValidationAction):
     ) -> Dict[Text, Any]:
         """Validate `class_name` value."""
 
+        print('start validating class name')
         if slot_value not in CLASS_NAMES:
             similarity_perc = [Levenshtein.jaro(slot_value.lower(), s.lower()) for s in CLASS_NAMES]
             if max(similarity_perc) < 0.7:
@@ -197,23 +232,6 @@ class ValidateClassNameForm(FormValidationAction):
                 slot_value = CLASS_NAMES[most_similar_index]
                 return {"class_name": slot_value}
         return {"class_name": slot_value}
-
-
-class AskForVegetarianAction(Action):
-    def name(self) -> Text:
-        return "action_ask_vegetarian"
-
-    def run(
-            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
-        dispatcher.utter_message(
-            text="Would you like to order a vegetarian pizza?",
-            buttons=[
-                {"title": "yes", "payload": "/affirm"},
-                {"title": "no", "payload": "/deny"},
-            ],
-        )
-        return []
 
 
 class AskForClassTypeAction(Action):
@@ -273,53 +291,59 @@ def return_class_start_time(class_name, class_type, weekday) -> Text:
     message = ''
     weekday_string = ''
     if weekday is not None:
+        weekday_string = ' on ' + weekday
         weekday = WEEKDAYS_EN_DE.get(weekday)
-        weekday_string = 'on ' + weekday
     df = perform_query(mode=MODE, looking_for='start_time', class_name=class_name,
                        class_type=class_type, weekday=weekday, con=sqa_con)
     if df.empty:
-        class_type_string = '' if class_type is None else class_type
+        class_type_string = '' if class_type is None else CLASS_TYPES_DE_EN.get(class_type)
         message = f"I'm sorry, there is no data about the starting time of the {class_type_string} {class_name}"
         message += weekday_string
-    for i, row in df.iterrows():
-        if row['week_day'] is not None:
-            print('the class type is', class_type)
-            print('the class type in english is', CLASS_TYPES_DE_EN.get(class_type))
-            message += f"The {CLASS_TYPES_DE_EN.get(class_type)} {class_name} starts at {row['start_time']} " \
-                       f"on {WEEKDAYS_DE_EN.get(row['week_day'])}."
+    else:
+        print(df.head())
+        for i, row in df.iterrows():
+            if row['week_day'] is not None:
+                print('the class type is', class_type)
+                print('the class type in english is', CLASS_TYPES_DE_EN.get(class_type))
+                if i == 0:
+                    message += f"The {CLASS_TYPES_DE_EN.get(class_type)} {class_name} starts at {row['start_time']} " \
+                           f"on {WEEKDAYS_DE_EN.get(row['week_day'])}"
+                else:
+                    message += f" and at {row['start_time']} on {WEEKDAYS_DE_EN.get(row['week_day'])}"
     print('the message is', message)
-    return message
+    return message + '.'
 
 
 def return_class_lecturer(class_name, class_type) -> Text:
     print('start return_class_lecturer')
     df = perform_query(mode=MODE, looking_for='lecturer', class_name=class_name,
                        class_type=class_type, con=sqa_con)
-    if df.iloc[0]['lecturer'] is None:
-        print('lecturer is None')
-    print(df.iloc[0]['lecturer'] is None)
-    if not df.empty:
-        if df.iloc[0]['lecturer'] is None:
-            message = f"I'm sorry there is no information about the lecturer of the' {CLASS_TYPES_DE_EN.get(class_type)} " \
-                      f"{class_name} in the database."
-        else:
-            message = f"The lecturer of the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} is {df.iloc[0]['lecturer']}."
+    if df.empty or df.iloc[0]['lecturer'] is None:
+        class_type_string = '' if class_type is None else CLASS_TYPES_DE_EN.get(class_type)
+        message = f"I'm sorry, there is no data about the lecturer of the {class_type_string} {class_name}"
     else:
-        message = f"I can't find the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} in the database."
+        message = f"The lecturer of the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} is {df.iloc[0]['lecturer']}."
     return message
 
 
 def return_class_location(class_name, class_type, weekday) -> Text:
     print('start return_class_location')
+    weekday_string = ''
     if weekday is not None:
+        weekday_string = ' on ' + weekday
         weekday = WEEKDAYS_EN_DE.get(weekday)
     df = perform_query(mode=MODE, looking_for='location', class_name=class_name,
                        class_type=class_type, weekday=weekday, con=sqa_con)
-    if not df.empty:
-        message = f"The {CLASS_TYPES_DE_EN.get(class_type)} {class_name} takes place in {df.iloc[0]['room']} {df.iloc[0]['floor'].strip()}," \
-                  f"{df.iloc[0]['building']}, {df.iloc[0]['facility']}. The address is {df.iloc[0]['address']}."
+    if df.empty:
+        class_type_string = '' if class_type is None else CLASS_TYPES_DE_EN.get(class_type)
+        message = f"I'm sorry, there is no data about the location of the {class_type_string} {class_name}"
+        message += weekday_string
     else:
-        message = f"I can't find the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} in the database."
+        if weekday_string == '' and df.iloc[0]['week_day'] is not None:
+            weekday_string = ' on ' + WEEKDAYS_DE_EN.get(df.iloc[0]['week_day'])
+        message = f"The {CLASS_TYPES_DE_EN.get(class_type)} {class_name} {weekday_string} takes place " \
+                  f"in {df.iloc[0]['room']} {df.iloc[0]['floor'].strip()}, " \
+                  f"{df.iloc[0]['building']}, {df.iloc[0]['facility']}. The address is {df.iloc[0]['address']}."
     return message
 
 
@@ -329,36 +353,36 @@ def return_exam_type(class_name) -> Text:
     if not df.empty:
         message = strip_tags(df.iloc[0]['txt'])
     else:
-        message = f"I can't find information about the {class_name} in the database."
+        message = f"I can't find information about {class_name} in the database."
     return message
 
 
-def return_class_is_about(class_name, tracker) -> Text:
-    print('start return_class_is_about for class name ', class_name)
+def return_open_question(class_name, tracker) -> Text:
+    print('start return_open_question for class name ', class_name)
     df = perform_query(mode=MODE, looking_for='class_is_about', class_name=class_name, con=sqa_con)
-    if not df.empty:
+    if df.empty:
+        message = f"I'm sorry, there is no data on this topic about {class_name} in the database."
+    else:
         context = strip_tags(df.iloc[0]['txt'])
         question = tracker.latest_message['text']
         message = qa(question, context, electra_model, electra_tokenizer)
-    else:
-        message = f"I can't find information about the {class_name} in the database."
     return message
 
 
 def return_class_moodle_link(class_name, class_type, weekday) -> Text:
     print('start return_class_moodle_link')
-    query = get_moodle_link_query % (class_name, class_type)
+    weekday_string = ''
     if weekday is not None:
+        weekday_string = ' on ' + weekday
         weekday = WEEKDAYS_EN_DE.get(weekday)
     df = perform_query(mode=MODE, looking_for='moodle_link', class_name=class_name,
                        class_type=class_type, weekday=weekday, con=sqa_con)
-    if not df.empty:
-        if df.iloc[0]['hyperlink'] is not None:
-            message = f"The moodle link for the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} is {df.iloc[0]['hyperlink']}."
-        else:
-            message = f"There is no data about the moodle link for the {class_type} {class_name}."
+    if df.empty or df.iloc[0]['hyperlink'] is None:
+        class_type_string = '' if class_type is None else CLASS_TYPES_DE_EN.get(class_type)
+        message = f"I'm sorry, there is no data about the moodle link of the {class_type_string} {class_name}"
+        message += weekday_string
     else:
-        message = f"I can't find the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} in the database."
+        message = f"The moodle link for the {CLASS_TYPES_DE_EN.get(class_type)} {class_name} {weekday_string} is {df.iloc[0]['hyperlink']}."
     return message
 
 
@@ -369,10 +393,9 @@ def choose_action(tracker, class_name, class_type, weekday):
     for event in reversed_events:
         if 'parse_data' in event.keys():
             intent_name = event['parse_data']['intent']['name']
-            print(intent_name)
             if intent_name != 'inform':
                 break
-    print('Outside of for loop, intent is', intent_name)
+    print('Intent is ', intent_name)
     if intent_name == 'ask_class_start_time':
         message = return_class_start_time(class_name, class_type, weekday)
     elif intent_name == 'ask_class_lecturer':
@@ -383,8 +406,8 @@ def choose_action(tracker, class_name, class_type, weekday):
         message = return_class_moodle_link(class_name, class_type, weekday)
     elif intent_name == 'ask_exam_type':
         message = return_exam_type(class_name)
-    elif intent_name == 'ask_class_is_about':
-        message = return_class_is_about(class_name, tracker)
+    elif intent_name == 'ask_open_question':
+        message = return_open_question(class_name, tracker)
     return message
 
 
